bb51d23d50a0e3d90221e9ee6fb2ff72f5cac210
Harsh <harsh.dhar543@gmail.com>
2024-02-18 00:52:44 +0530

    User Infomation UI

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.css']
+  styleUrls: ['./user-info.component.css'],
 })
 export class UserInfoComponent {
+  constructor() {}
 
+  @Input() userData: any = {};
+  @Input() followers: any = [];
+  @Input() followings: any = [];
+
+  openProfile(username: string) {
+    window.open(`https://github.com/${username}`, '_blank');
+  }
 }
