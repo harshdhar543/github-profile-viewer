8d081dbecf65b1812b849ca300947eb748a2926c
Harsh <harsh.dhar543@gmail.com>
2024-02-17 21:29:44 +0530

    User card for search results

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  styleUrls: ['./user-card.component.css'],
 })
 export class UserCardComponent {
+  constructor(private router: Router) {}
 
+  @Input() username!: string;
+  @Input() name!: string;
+  @Input() avatar!: string;
+  @Input() type!: string;
+
+  ngOnInit() {}
+
+  goToProfile(userId: string): void {
+    this.router.navigate(['Profile', userId]);
+  }
 }
