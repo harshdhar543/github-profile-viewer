eabd7bdc48f3a26ff23df11104e0253ba0f75fd0
Harsh <harsh.dhar543@gmail.com>
2024-02-18 00:51:14 +0530

    API function updates

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -8,15 +8,24 @@ import { Observable } from 'rxjs';
 export class GithubService {
   constructor(private http: HttpClient) {}
 
-  searchUsers(searchString: string): Observable<any>{
-    return this.http.get(`https://api.github.com/search/users?q=${searchString}&per_page=100`)
+  searchUsers(searchString: string): Observable<any> {
+    return this.http.get(
+      `https://api.github.com/search/users?q=${searchString}&per_page=100`
+    );
   }
 
   getUserDetails(username: string) {
     return this.http.get(`https://api.github.com/users/${username}`);
   }
 
-  getRepos(repoUrl: string) {
-    return this.http.get(repoUrl);
+  getRepositories(username: string | null) {
+    return this.http.get(`https://api.github.com/users/${username}/repos`);
+  }
+
+  getFollowings(username: string | null): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${username}/following`);
+  }
+  getFollowers(username: string | null): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${username}/followers`);
   }
 }
