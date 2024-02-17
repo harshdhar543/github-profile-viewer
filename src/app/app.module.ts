75238d347d848721c5200e356cbcb656efc9f341
Harsh <harsh.dhar543@gmail.com>
2024-02-17 13:59:52 +0530

    Project structure changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { RepoCardComponent } from './components/repo-card/repo-card.component';
+import { RepoListComponent } from './components/repo-list/repo-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GithubComponent } from './service/github/github.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    UserInfoComponent,
+    RepoCardComponent,
+    RepoListComponent,
+    HomeComponent,
+    ProfileComponent,
+    GithubComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/repo-card/repo-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-card',
+  templateUrl: './repo-card.component.html',
+  styleUrls: ['./repo-card.component.css']
+})
+export class RepoCardComponent {
+
+}
