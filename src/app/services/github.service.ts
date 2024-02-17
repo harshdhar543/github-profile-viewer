9c4981fc6e21891c9316cd484ed098c75a561d99
Harsh <harsh.dhar543@gmail.com>
2024-02-17 21:28:54 +0530

    GitHub API integration

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  searchUsers(searchString: string): Observable<any>{
+    return this.http.get(`https://api.github.com/search/users?q=${searchString}&per_page=100`)
+  }
+
+  getUserDetails(username: string) {
+    return this.http.get(`https://api.github.com/users/${username}`);
+  }
+
+  getRepos(repoUrl: string) {
+    return this.http.get(repoUrl);
+  }
 }
