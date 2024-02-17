b3702a8c87f1e3cd26144761ee5109288be18921
Harsh <harsh.dhar543@gmail.com>
2024-02-18 00:51:53 +0530

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { SideNavComponent } from './layout/side-nav/side-nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FollowerCardComponent } from './components/follower-card/follower-card.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ProfileComponent,
     HeaderComponent,
     FooterComponent,
-    SideNavComponent
+    SideNavComponent,
+    FollowerCardComponent
   ],
   imports: [
     BrowserModule,
